393aaea55d9fa6d500e98dc433a6b9e61b3ffdf9
BOT22345 <nmongia48@gmail.com>
2024-09-02 10:39:49 -0700

    added the reset button

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//Variables
+let score_A=document.getElementById("A_score");
+let score_B=document.getElementById("B_score");
+let Turn=true;
 
-let score_A=document.getElementById("A_score").innerText=0;
-let score_B=document.getElementById("B_score").innerText=0;
 // to change the turn 
-
 function click1(){
     let turn2=document.getElementById("playerTurn");
     if(turn2.innerText==="Player A turn"){
@@ -13,8 +14,32 @@ function click1(){
     }   
 }
 
+//score Changer for A
+function AScoreAdd(){
+    score_A.innerText++;
+}
+//score Changer for B
+function BScoreAdd(){
+    score_B.innerText++; 
+}
+
+//reset button function
+function ButtonReset(){
+    let Cells=document.getElementsByClassName("cell");
+    for(let i=0;i<Cells.length;i++){
+        Cells[i].innerText="_"
+    }
+
+}
 
 
 
+//Event listners
+let gameboard=document.getElementById("gameboard");
+gameboard.addEventListener("click",()=>{
+    click1();
+    AScoreAdd();
+    BScoreAdd();
+})
 
 
