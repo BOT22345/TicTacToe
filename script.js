a1f36c3940167db8144cf1d050650468728e76ce
BOT22345 <nmongia48@gmail.com>
2024-09-02 11:47:41 -0700

    added the score reset button

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let Turn=true;
 // to change the turn 
 function click1(){
     let turn2=document.getElementById("playerTurn");
-    if(turn2.innerText==="Player A turn"){
-        turn2.innerText="Player B turn";
+    if(turn2.innerText==="Player A :X"){
+        turn2.innerText="Player B :O";
     }
     else{
-        turn2.innerText="Player A turn";
+        turn2.innerText="Player A :X";
     }   
 }
 
@@ -23,23 +23,29 @@ function BScoreAdd(){
     score_B.innerText++; 
 }
 
-//reset button function
-function ButtonReset(){
+//reset board function
+function BoardReset(){
     let Cells=document.getElementsByClassName("cell");
     for(let i=0;i<Cells.length;i++){
         Cells[i].innerText="_"
     }
+}
 
+//reset Score
+function ScoreReset(){
+    score_A.innerText=" ";
+    score_B.innerText=" ";
 }
 
 
 
-//Event listners
+// Event listners
 let gameboard=document.getElementById("gameboard");
 gameboard.addEventListener("click",()=>{
     click1();
     AScoreAdd();
     BScoreAdd();
 })
+
 
 
